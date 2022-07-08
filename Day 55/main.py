9dc63e8ecb8936e60b8a749fbc9f1ba067e2a672
mattrende <mrende@gmail.com>
2022-07-08 15:00:16 -0700

    Day 55

---
+++ Day 55/main.py
@@ -0,0 +1,36 @@
+from flask import Flask
+app = Flask(__name__)
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
+@app.route('/')
+@make_bold
+@make_emphasis
+@make_underlined
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/bye')
+def bye():
+    return 'bye'
+
+@app.route('/username/<name>')
+def greet(name):
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
